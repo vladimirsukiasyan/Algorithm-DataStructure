c64b31cb987beb92a5941e26e6688283aa6535ab
vladimirsukiasyan <vova_1999-99@mail.ru>
2020-07-06 20:33:15 +0300

    Some change in Stack

--- lesson2/src/Stack.kt
+++ lesson2/src/Stack.kt
@@ -1,8 +1,8 @@
-class ListNode(var value:Int = -1, var next: ListNode? = null){
+class ListNode(var value:Int = -1, var next: ListNode? = null) {
     override fun toString(): String {
         return value.toString()
     }
-
+}
 
 class Stack(var head: ListNode? = null){
     fun push(newNode: ListNode){
